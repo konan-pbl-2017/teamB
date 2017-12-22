6527f6c98b45b738a4a8fe32c9c05e4935c0c114
s1671070 <s1671070@PC132G055.ku-kinds.konan-u.ac.jp>
2017-12-22 14:12:55 +0900

    敵の出現位置変更　敵のスピード変更

--- teamB/src/game/Enemy.java
+++ teamB/src/game/Enemy.java
@@ -13,7 +13,7 @@ import framework.game2D.Velocity2D;
  * 
  */
 public class Enemy extends OvergroundActor2D {
-	private static final double ENEMY_SPEED = 2.75;
+	private static final double ENEMY_SPEED = 3.5;
 
 	@Override
 	public String getAnimationFileName() {
