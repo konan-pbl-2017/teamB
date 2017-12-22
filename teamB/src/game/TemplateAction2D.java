220784cec643e5d9a3985ea4f6ce88e85ba8954f
s1671002 <s1671002@PC132G040.ku-kinds.konan-u.ac.jp>
2017-12-22 13:13:05 +0900

    アイテム拡張しました

--- teamB/src/game/TemplateAction2D.java
+++ teamB/src/game/TemplateAction2D.java
@@ -1,14 +1,26 @@
 package game;
 
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.GraphicsConfiguration;
 import java.util.ArrayList;
 
 import javax.vecmath.Vector2d;
 
+import template.shooting2D.StartContainer;
+import template.shooting2D.TemplateShooting2DMultiStates;
+
+import framework.RWT.RWTBoard;
+import framework.RWT.RWTCanvas3D;
+import framework.RWT.RWTContainer;
 import framework.RWT.RWTFrame3D;
+import framework.RWT.RWTLabel;
 import framework.RWT.RWTVirtualController;
+import framework.RWT.RWTVirtualKey;
 import framework.game2D.Ground2D;
 import framework.game2D.OvergroundActor2D;
 import framework.game2D.Velocity2D;
+import framework.gameMain.IGameState;
 import framework.gameMain.SimpleActionGame;
 import framework.model3D.Object3D;
 import framework.model3D.Universe;
@@ -21,7 +33,10 @@ public class TemplateAction2D extends SimpleActionGame {
 	private ArrayList<Item2> items2 = new ArrayList<Item2>();
 	private ArrayList<Item3> items3 = new ArrayList<Item3>();
 	private Ground2D stage;
-
+	int count=128;
+	/*RWTLabel scoreLabel;
+	RWTBoard scoreBoard;
+	*/
 	// あとで設計変更
 	// Enemyクラスでこの値を使いたいため。
 	public static final int RANGE = 40;
@@ -29,6 +44,9 @@ public class TemplateAction2D extends SimpleActionGame {
 	// プレイヤーの現在の速度が代入されるグローバル変数
 	private Velocity2D curV;
 
+
+
+	
 	@Override
 	public void init(Universe universe) {
 		player = new Player();
@@ -44,7 +62,7 @@ public class TemplateAction2D extends SimpleActionGame {
 		universe.place(enemy); // universeに置く。後で取り除けるようにオブジェクトを配置する。
 		
 		Item item = new Item();//itemの設置
-		item.setPosition(2.0, 3.0);
+		item.setPosition(-24.0, 16.0);
 		item.setDirection(1.0, 0.0);
 		((Object3D) item.getBody()).scale(0.01);
 		universe.place(item); // universeに置く。後で取り除けるようにオブジェクトを配置する。
@@ -52,18 +70,68 @@ public class TemplateAction2D extends SimpleActionGame {
 		
 		
 		Item2 item2 = new Item2();//item2の設置
-		item2.setPosition(5.0, 6.0);
+		item2.setPosition(0.0, 6.0);
 		item2.setDirection(1.0, 0.0);
 		((Object3D) item2.getBody()).scale(0.01);
 		universe.place(item2); // universeに置く。後で取り除けるようにオブジェクトを配置する。
 		items2.add(item2);
 		
 		Item3 item3 = new Item3();//item3の設置
-		item3.setPosition(12.0, 3.0);
+		item3.setPosition(-56.0, 16.0);
 		item3.setDirection(1.0, 0.0);
 		((Object3D) item3.getBody()).scale(0.01);
 		universe.place(item3); // universeに置く。後で取り除けるようにオブジェクトを配置する。
 		items3.add(item3);
+		
+		
+   //アイテム設置
+		//アイテム1
+		Item item21 = new Item();//itemの設置
+		item21.setPosition(-14.0, 3.0);
+		item21.setDirection(1.0, 0.0);
+		((Object3D) item21.getBody()).scale(0.01);
+		universe.place(item21); // universeに置く。後で取り除けるようにオブジェクトを配置する。
+		items.add(item21);
+		
+		Item item31 = new Item();//itemの設置
+		item31.setPosition(-32.0, 6.0);
+		item31.setDirection(1.0, 0.0);
+		((Object3D) item31.getBody()).scale(0.01);
+		universe.place(item31); // universeに置く。後で取り除けるようにオブジェクトを配置する。
+		items.add(item31);
+		
+				//アイテム2
+
+		Item2 item22 = new Item2();//item2の設置
+		item22.setPosition(-64.0, 20.0);
+		item22.setDirection(1.0, 0.0);
+		((Object3D) item22.getBody()).scale(0.01);
+		universe.place(item22); // universeに置く。後で取り除けるようにオブジェクトを配置する。
+		items2.add(item22);
+		
+
+		Item2 item32 = new Item2();//item2の設置
+		item32.setPosition(15.0, 6.0);
+		item32.setDirection(1.0, 0.0);
+		((Object3D) item32.getBody()).scale(0.01);
+		universe.place(item32); // universeに置く。後で取り除けるようにオブジェクトを配置する。
+		items2.add(item32);
+		
+		//アイテム3
+		Item3 item23 = new Item3();//item3の設置
+		item23.setPosition(-74.0, 20.0);
+		item23.setDirection(1.0, 0.0);
+		((Object3D) item23.getBody()).scale(0.01);
+		universe.place(item23); // universeに置く。後で取り除けるようにオブジェクトを配置する。
+		items3.add(item23);
+		
+		
+		Item3 item33 = new Item3();//item3の設置
+		item33.setPosition(-84.0, 19.0);
+		item33.setDirection(1.0, 0.0);
+		((Object3D) item33.getBody()).scale(0.01);
+		universe.place(item33); // universeに置く。後で取り除けるようにオブジェクトを配置する。
+		items3.add(item33);
 		
 		// ステージの3Dデータを読み込み配置する
 		stage = new Ground2D("data\\images\\stage.obj",
@@ -83,6 +151,7 @@ public class TemplateAction2D extends SimpleActionGame {
 		f.setTitle("Template for Action 2DGame");
 		return f;
 	}
+
 
 	@Override
 	public void progress(RWTVirtualController virtualController, long interval) {
@@ -132,6 +201,9 @@ public class TemplateAction2D extends SimpleActionGame {
 				universe.displace(item);
 				items.remove(i);
 				i--;
+				count++;
+				System.out.println("現在のポイントは"+count);
+				
 			}
 		}
 
@@ -143,6 +215,8 @@ public class TemplateAction2D extends SimpleActionGame {
 				universe.displace(item2);
 				items2.remove(j);
 				j--;
+				count+=2;
+				System.out.println("現在のポイントは"+count);
 			}
 		}
 
@@ -154,22 +228,64 @@ public class TemplateAction2D extends SimpleActionGame {
 				universe.displace(item3);
 				items3.remove(j);
 				j--;
+				count+=3;
+				System.out.println("現在のポイントは"+count);
 			}
 		}
 	}
-	
+	/*
+	protected RWTContainer createRWTContainer() {
+		return new RWTContainer() {
+			RWTCanvas3D canvas;
 
-	/**
+			@Override
+			public void build(GraphicsConfiguration gc) {
+				if (gc != null) {
+					canvas = new RWTCanvas3D(gc);
+				} else {
+					canvas = new RWTCanvas3D();
+				}
+				canvas.setRelativePosition(0.0f, 0.0f);
+				canvas.setRelativeSize(1.0f, 1.0f);
+				
+				scoreBoard = new RWTBoard(0.2f, 0.2f, 0.2f, 0.05f, Color.WHITE);
+				scoreBoard.setVisible(true);
+				canvas.addWidget(scoreBoard);
+				scoreLabel = new RWTLabel(0.5f, 0.2f, "score"+count, Color.RED, new Font("", Font.ITALIC, 50));
+				scoreLabel.setVisible(true);
+				canvas.addWidget(scoreLabel);
+
+				addCanvas(canvas);
+				repaint();
+			}
+
+			// RWT側ではイベント処理をしない
+			@Override
+			public void keyPressed(RWTVirtualKey key) {
+			}
+
+			@Override
+			public void keyReleased(RWTVirtualKey key) {
+			}
+
+			@Override
+			public void keyTyped(RWTVirtualKey key) {
+			}
+		};
+	}
+	*/
+	/*
 	 * ゲームのメイン
 	 * 
 	 * @param args
 	 */
-	public static void main(String[] args) {
+public static void main(String[] args) {
 		TemplateAction2D game = new TemplateAction2D();
 		game.setFramePolicy(5, 33, false);
 		game.start();
-	}
-
+}
+	
+		
 	@Override
 	public OvergroundActor2D getOvergroundActor() {
 		return player;
